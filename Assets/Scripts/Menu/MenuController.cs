cf4713177bc576f0aeababd6fee757e268b293b4
Mark <markel_quevedo@euneiz.com>
2025-02-09 19:31:59 +0100

    Menus Normal y Pausa

--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -7,7 +7,17 @@ public class MenuController : MonoBehaviour
 {
     public GameObject Menu;
     public GameObject Player;
+    public GameObject Opciones;
+    public GameObject MenuP;
+    public GameObject MenuPause;
     // Método para salir del juego
+    void Start()
+    {
+        MenuP.SetActive(true);
+        Player.SetActive(false);
+        Opciones.SetActive(false);
+        MenuPause.SetActive(false);
+    }
     public void ExitGame()
     {
         Debug.Log("Saliendo del juego..."); // Mensaje en la consola para verificar que funciona
@@ -22,5 +32,16 @@ public class MenuController : MonoBehaviour
     {
         Menu.SetActive(false);
         Player.SetActive(true);
+        MenuPause.SetActive(false);
+    }
+    public void OptionsGame()
+    {
+        Opciones.SetActive(true);
+        MenuP.SetActive(false);
+    }
+    public void OptionsVolver()
+    {
+        MenuP.SetActive(true);
+        Opciones.SetActive(false);
     }
 }
