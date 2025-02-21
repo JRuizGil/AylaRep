3e697a8a0f1b52849fb73b06decc10ed5a73b5c5
JRuizG <joseramon_ruiz@euneiz.com>
2025-02-21 18:29:18 +0100

    MapFix Ingredientesounter

--- Assets/Scripts/Hability/ObtainIngrediente.cs
+++ Assets/Scripts/Hability/ObtainIngrediente.cs
@@ -5,41 +5,47 @@ using UnityEngine.UI;
 
 public class ObtainIngrediente : MonoBehaviour
 {
-    private int ingredientes = 0;
-    public bool isInsideTrigger;
+    private int ingredientes;
     public GameObject Mariposa;
     public GameObject Counterobj;
     public Text Counter;
     // Start is called before the first frame update
     void Start()
     {
+        Mariposa.SetActive(true);
         Counterobj.SetActive(false);
-        isInsideTrigger = false;
+        ingredientes = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isInsideTrigger)
+        if (Counterobj.activeSelf)
         {
-            Mariposa.SetActive(false);
+            Counter.text = ingredientes.ToString();
         }
-        Counter.text = ingredientes.ToString();
-
     }
+
     void OnTriggerEnter2D(Collider2D bola)
     {
         if (bola.CompareTag("Player"))
         {
-            isInsideTrigger = true;
+            ingredientes++;
             StartCoroutine(ActivarTemporalmente());
+            Mariposa.SetActive(false);
         }
-
     }
+
     private IEnumerator ActivarTemporalmente()
     {
         Counterobj.SetActive(true);
+        Debug.Log("Mostrando contador...");
+
         yield return new WaitForSeconds(5);
+
         Counterobj.SetActive(false);
+        Debug.Log("Ocultando contador...");
     }
+
+
 }
